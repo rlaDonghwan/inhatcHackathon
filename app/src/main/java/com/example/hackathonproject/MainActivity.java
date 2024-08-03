50b66c0a1df96b77c660b3e1e1a106787942a408
donghwan <kdhe9797@naver.com>
2024-08-04 01:58:15 +0900

    로그인화면 UI 구성 미완성

--- app/src/main/java/com/example/hackathonproject/MainActivity.java
+++ app/src/main/java/com/example/hackathonproject/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.hackathonproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
 
-        // 메인 화면이 나타날 때 간단한 메시지 표시
         Toast.makeText(this, "메인 화면이 표시되었습니다.", Toast.LENGTH_SHORT).show();
+
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignInPhoneNumActivity.class);
+            startActivity(intent);
+        });
     }
 }
